72dab9f6bb04ecb4b2a598f868fd12ec978b3a3c
tacone <tacone@users.noreply.github.com>
2014-10-25 23:07:04 +0200

    fix helpers.php parse error, move to psr4, add tests

---
+++ tests/DataSetTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Zofe\DataGrid\Tests;
+
+use Zofe\DataGrid\DataSet;
+
+class DataSetTest extends BaseTestCase
+{
+    public function testInstantiation()
+    {
+        $datagrid = new DataSet();
+    }
+
+}
